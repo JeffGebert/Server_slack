c921d85ac8c6aff7d07f1afd2a56cc02d9bdbe1c
JeffGebert <jeffgebert@hotmail.com>
2019-07-08 04:35:37 -0400

    resolved channel switching
    Signed-off-by: JeffGebert <jeffgebert@hotmail.com>

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res) => {
 	console.log('test',req.headers)
 	let token = req.headers.authorization.split(' ')[1]
+	console.log(token)
 	jwt.verify(req.body.token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
 			console.log('decoded', decoded)
